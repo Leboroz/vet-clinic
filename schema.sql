2f8212d91d74ece8ef36d0464f4fa92d0efcb365
Leonardo <albornozleo95@gmail.com>
2022-03-21 13:30:51 -0400

    Create table animal

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+vet_clinic=# CREATE TABLE animals(
+vet_clinic(# ID INT,
+vet_clinic(# NAME TEXT NOT NULL,
+vet_clinic(# DATE_OF_BIRTH DATE NOT NULL,
+vet_clinic(# ESCAPE_ATTEMPTS INT NOT NULL,
+vet_clinic(# NEUTERED BOOLEAN NOT NULL,
+vet_clinic(# WEIGHT_KG DECIMAL NOT NULL
+vet_clinic(# );

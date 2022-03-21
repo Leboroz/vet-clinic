7371f99f59f08b9415533749b9a65e55e573a7be
Leonardo <albornozleo95@gmail.com>
2022-03-21 14:08:49 -0400

    Insert animals

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-vet_clinic=# CREATE TABLE animals(
-vet_clinic(# ID INT,
-vet_clinic(# NAME TEXT NOT NULL,
-vet_clinic(# DATE_OF_BIRTH DATE NOT NULL,
-vet_clinic(# ESCAPE_ATTEMPTS INT NOT NULL,
-vet_clinic(# NEUTERED BOOLEAN NOT NULL,
-vet_clinic(# WEIGHT_KG DECIMAL NOT NULL
-vet_clinic(# );
+CREATE TABLE animals(
+  ID INT,
+  NAME TEXT NOT NULL,
+  DATE_OF_BIRTH DATE NOT NULL,
+  ESCAPE_ATTEMPTS INT NOT NULL,
+  NEUTERED BOOLEAN NOT NULL,
+  WEIGHT_KG DECIMAL NOT NULL
+);

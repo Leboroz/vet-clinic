d2fc15dcaa1dd0794b53b19a9ae399b9c15232b1
Leonardo <albornozleo95@gmail.com>
2022-03-22 15:26:38 -0400

    Add inserts

--- data.sql
+++ data.sql
@@ -12,3 +12,13 @@ VALUES (2, 'Pikachu', '2021-01-07', 1, false, 15.04);
 INSERT INTO animals
 VALUES (3, 'Devimon', '2017-05-12', 5, true, 11);
 
+INSERT INTO animals
+ VALUES ('Charmander', '2020-02-08', 0, true, -11),
+ ('Plantmon', '2021-11-15', 2, true, -5.7),
+ ('Squirtle', '1993-04-02', 3, fase, -12.13),
+ ('Angemon', '2005-06-12', 1, true, -45),
+ ('Boardmon', '2005-06-07', 7, true, 20.4),
+ ('Blossom', '1998-10-13', 3, true, 17),
+ ('Ditto', '2022-05-14', 4, true, 22);
+
+
